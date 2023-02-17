158743a88454057e373d46fa7b3981f190680628
NaDo2383 <narandorj.byambadorj@gmail.com>
2023-02-17 14:51:24 +0800

    20230217 nado

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -14,7 +14,7 @@ function Home() {
     return (
         <div>
             <Services />
-            {/* <InfoSection /> */}
+            <InfoSection />
             <Greeting />
             <ProjectSection />
             <BusinessFacts />
